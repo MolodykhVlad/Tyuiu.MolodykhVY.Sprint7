fa4013f93b0f8c3ba01e8bdf5823513319e61df3
MolodykhVlad <Mr.Prins@yandex.ru>
2023-12-12 11:47:12 +0300

    ButtonLeftPanel

--- Tyuiu.MolodykhVY.Sprint7.V4/FormMain.Designer.cs
+++ Tyuiu.MolodykhVY.Sprint7.V4/FormMain.Designer.cs
@@ -29,27 +29,47 @@ namespace Tyuiu.MolodykhVY.Sprint7.V4
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.ButtonLeftPanel = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtonLeftPanel)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(14)))), ((int)(((byte)(14)))));
+            this.panel1.Controls.Add(this.ButtonLeftPanel);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1098, 118);
+            this.panel1.Size = new System.Drawing.Size(732, 77);
             this.panel1.TabIndex = 0;
+            // 
+            // ButtonLeftPanel
+            // 
+            this.ButtonLeftPanel.Image = ((System.Drawing.Image)(resources.GetObject("ButtonLeftPanel.Image")));
+            this.ButtonLeftPanel.Location = new System.Drawing.Point(12, 12);
+            this.ButtonLeftPanel.Name = "ButtonLeftPanel";
+            this.ButtonLeftPanel.Size = new System.Drawing.Size(50, 50);
+            this.ButtonLeftPanel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ButtonLeftPanel.TabIndex = 0;
+            this.ButtonLeftPanel.TabStop = false;
+            this.ButtonLeftPanel.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1098, 620);
+            this.ClientSize = new System.Drawing.Size(732, 403);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormMain";
             this.Text = "Library";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ButtonLeftPanel)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +77,7 @@ namespace Tyuiu.MolodykhVY.Sprint7.V4
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox ButtonLeftPanel;
     }
 }
 
--- Tyuiu.MolodykhVY.Sprint7.V4/FormMain.cs
+++ Tyuiu.MolodykhVY.Sprint7.V4/FormMain.cs
@@ -16,5 +16,10 @@ namespace Tyuiu.MolodykhVY.Sprint7.V4
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
